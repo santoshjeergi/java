8c82eed05c3788182bbb6b1a3458d5db69e286e7
santosh jeergi <santoshjeergi@gmail.com>
2023-07-01 15:39:45 +0530

    SOLID1

--- JavaWSgitCode/src/DesignPatterns/SOLID/Client.java
+++ JavaWSgitCode/src/DesignPatterns/SOLID/Client.java
@@ -2,8 +2,13 @@ package DesignPatterns.SOLID;
 
 public class Client {
 
-    Flayator fr = new fastFlayator();
-   // Bird b = new Sparrow(fr);
-    Sparrow s = new Sparrow(fr);
-    s.fly()
+    public static void main(String[] args) {
+        Flayator fr = new fastFlayator();
+         //Bird b = new Sparrow(fr);
+        Sparrow s = new Sparrow(new slowFlayator());
+        s.fly();
+        s.setFref(fr);
+        s.fly();
+    }
+
 }
